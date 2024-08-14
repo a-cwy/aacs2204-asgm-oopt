f37094b5c88375102ba6d257fe9f9c07a918c6fb
Tan Jun Yew <im.a.life.thuger.bro@gmail.com>
2024-08-14 18:32:54 +0800

    done data fields and constructor

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,22 @@
 public class Product {
+    private String id;
+    private String name;
+    private String brand;
+    private double price;
+    private int quantity;
+
+    Product(String id, String name, String brand) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+    }
+
+    Product(String id, String name, String brand, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
 }
