1294bbf567e4a9b46fa50cee5c5ca453bab9d55c
Tan Jun Yew <im.a.life.thuger.bro@gmail.com>
2024-08-15 12:34:22 +0800

    done methods

--- src/Product.java
+++ src/Product.java
@@ -19,4 +19,57 @@ public class Product {
         this.quantity = quantity;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void addQuantity(int amt) {
+        this.quantity += amt;
+    }
+
+    public void reduceQuantity(int amt) {
+        this.quantity -= amt;
+    }
+
+    public double totalValue() {
+        return price * quantity;
+    }
+
+    public double valueOf(int amt) {
+        return price * amt / quantity;
+    }
+
+    public String toString() {
+        return "ID: " + id + "\nName: " + name + "\nBrand: " + brand +
+                "\nPrice: " + price + "\nQuantity: " + quantity;
+    }
+
 }
